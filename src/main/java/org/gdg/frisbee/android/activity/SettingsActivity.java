f840536e27d8a41c7270afdcd2b74a8017428e09
Sebastian Mauer <mauimauer@gmail.com>
2013-06-25 21:19:35 +0200

    Add Google Analytics SDK to pom.xml

---
+++ src/main/java/org/gdg/frisbee/android/activity/SettingsActivity.java
@@ -0,0 +1,12 @@
+package org.gdg.frisbee.android.activity;
+
+/**
+ * GDG Aachen
+ * org.gdg.frisbee.android.activity
+ * <p/>
+ * User: maui
+ * Date: 25.06.13
+ * Time: 21:15
+ */
+public class SettingsActivity {
+}
