6a473e19401e3da092e92940d83a115eebfcc917
Sebastian Mauer <mauimauer@gmail.com>
2013-07-03 03:51:40 +0200

    Add icons for navigation drawer

---
+++ app/src/main/java/org/gdg/frisbee/android/adapter/DrawerAdapter.java
@@ -0,0 +1,11 @@
+package org.gdg.frisbee.android.adapter;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: maui
+ * Date: 03.07.13
+ * Time: 02:50
+ * To change this template use File | Settings | File Templates.
+ */
+public class DrawerAdapter {
+}
