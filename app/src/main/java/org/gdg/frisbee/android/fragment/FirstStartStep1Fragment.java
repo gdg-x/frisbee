1a08cb9137ef81541e05435770f4dd03997e15c5
Bartek Przybylski <bart.p.pl@gmail.com>
2013-09-01 02:15:44 +0200

    Make initial gdg home selection experience improvement

--- app/src/main/java/org/gdg/frisbee/android/fragment/FirstStartStep1Fragment.java
+++ app/src/main/java/org/gdg/frisbee/android/fragment/FirstStartStep1Fragment.java
@@ -16,16 +16,15 @@
 
 package org.gdg.frisbee.android.fragment;
 
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.Spinner;
+import android.widget.ListView;
 import android.widget.ViewSwitcher;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
@@ -34,42 +33,36 @@ import com.google.android.gms.plus.GooglePlusUtil;
 import de.keyboardsurfer.android.widget.crouton.Crouton;
 import de.keyboardsurfer.android.widget.crouton.Style;
 import org.gdg.frisbee.android.R;
-import org.gdg.frisbee.android.adapter.ChapterAdapter;
 import org.gdg.frisbee.android.api.ApiRequest;
 import org.gdg.frisbee.android.api.GroupDirectory;
 import org.gdg.frisbee.android.api.model.Chapter;
 import org.gdg.frisbee.android.api.model.Directory;
 import org.gdg.frisbee.android.app.App;
 import org.gdg.frisbee.android.cache.ModelCache;
-import org.gdg.frisbee.android.utils.ChapterComparator;
-import org.gdg.frisbee.android.utils.GingerbreadLastLocationFinder;
 import org.joda.time.DateTime;
 import roboguice.inject.InjectView;
+
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 public class FirstStartStep1Fragment extends RoboSherlockFragment {
 
-    private static String LOG_TAG = "GDG-FirstStartStep1Fragment";
+    private static final String LOG_TAG = "GDG-FirstStartStep1Fragment";
+
+    private static final int SWITCHER_CHILD_BUTTON = 1;
 
     private ApiRequest mFetchChaptersTask;
-    private ChapterAdapter mSpinnerAdapter;
     private GroupDirectory mClient;
-
     private Chapter mSelectedChapter;
 
-    @InjectView(R.id.chapter_spinner)
-    Spinner mChapterSpinner;
+    @InjectView(R.id.chapter_list)
+    private ListView mChapterListView;
 
     @InjectView(R.id.confirm)
     Button mConfirm;
 
     @InjectView(R.id.viewSwitcher)
     ViewSwitcher mLoadSwitcher;
-
-    private SharedPreferences mPreferences;
-    private ChapterComparator mLocationComparator;
 
     public static FirstStartStep1Fragment newInstance() {
         Log.d(LOG_TAG, "newInstance");
@@ -84,9 +77,8 @@ public class FirstStartStep1Fragment extends RoboSherlockFragment {
         Log.d(LOG_TAG, "onSaveInstanceState");
         super.onSaveInstanceState(outState);
 
-        if(mSpinnerAdapter != null && mSpinnerAdapter.getCount() > 0)
-            outState.putParcelable("selected_chapter", mSpinnerAdapter.getItem(mChapterSpinner.getSelectedItemPosition()));
-
+        if(mSelectedChapter != null)
+            outState.putParcelable("selected_chapter", mSelectedChapter);
     }
 
 
@@ -101,15 +93,12 @@ public class FirstStartStep1Fragment extends RoboSherlockFragment {
             GooglePlusUtil.getErrorDialog(errorCode, getActivity(), 0).show();
         }
 
-        mPreferences = getActivity().getSharedPreferences("gdg", Context.MODE_PRIVATE);
-        mLocationComparator = new ChapterComparator(mPreferences);
-
         mClient = new GroupDirectory();
-        mSpinnerAdapter = new ChapterAdapter(getActivity(), android.R.layout.simple_list_item_1);
 
-        if(savedInstanceState != null) {
+        if(savedInstanceState != null)
             mSelectedChapter = savedInstanceState.getParcelable("selected_chapter");
-        }
+
+        mConfirm.setEnabled(mSelectedChapter != null);
 
         mFetchChaptersTask = mClient.getDirectory(new Response.Listener<Directory>() {
                   @Override
@@ -118,7 +107,8 @@ public class FirstStartStep1Fragment extends RoboSherlockFragment {
                           @Override
                           public void onPutIntoCache() {
                               addChapters(directory.getGroups());
-                              mLoadSwitcher.setDisplayedChild(1);
+
+                              mLoadSwitcher.setDisplayedChild(SWITCHER_CHILD_BUTTON);
                           }
                       });
 
@@ -127,7 +117,7 @@ public class FirstStartStep1Fragment extends RoboSherlockFragment {
                   @Override
                   public void onErrorResponse(VolleyError volleyError) {
                       Crouton.makeText(getActivity(), getString(R.string.fetch_chapters_failed), Style.ALERT).show();
-                      Log.e(LOG_TAG, "Could'nt fetch chapter list", volleyError);
+                      Log.e(LOG_TAG, "Couldn't fetch chapter list", volleyError);
                   }
               });
 
@@ -135,7 +125,7 @@ public class FirstStartStep1Fragment extends RoboSherlockFragment {
             @Override
             public void onGet(Object item) {
                 Directory directory = (Directory) item;
-                mLoadSwitcher.setDisplayedChild(1);
+                mLoadSwitcher.setDisplayedChild(SWITCHER_CHILD_BUTTON);
                 addChapters(directory.getGroups());
             }
 
@@ -145,45 +135,68 @@ public class FirstStartStep1Fragment extends RoboSherlockFragment {
             }
         });
 
+        mChapterListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                mConfirm.setEnabled(true);
+                mSelectedChapter = (Chapter) mChapterListView.getItemAtPosition(position);
+            }
+        });
+
         mConfirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Chapter selectedChapter = (Chapter) mChapterSpinner.getSelectedItem();
-                getArguments().putParcelable("selected_chapter", selectedChapter);
+                getArguments().putParcelable("selected_chapter", mSelectedChapter);
 
                 if (getActivity() instanceof Step1Listener)
-                    ((Step1Listener) getActivity()).onConfirmedChapter(selectedChapter);
+                    ((Step1Listener) getActivity()).onConfirmedChapter(mSelectedChapter);
             }
         });
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
+    private int findSelectedChapterPosition() {
+        if (mSelectedChapter == null)
+            return -1;
+
+        Log.d(LOG_TAG, "Searing selected chapter from " + mChapterListView.getCount() + " entries");
+        for (int i = 0; i < mChapterListView.getCount(); ++i) {
+            if (mSelectedChapter.compareTo((Chapter)mChapterListView.getItemAtPosition(i)) == 0)
+                return i;
+        }
+        return -1;
+    }
+
+    private void scrollToSelectedChapter(int position) {
+        Log.d(LOG_TAG, "Scrolling to position " + position);
+        if (mChapterListView.getCount() == 0 || position == -1)
+            return;
+        int item_size = mChapterListView.getAdapter().getView(0, null, mChapterListView).getMeasuredHeight();
+        Log.d(LOG_TAG, "Item size is " + item_size);
+        // We want to show selected item somewhere in the middle, unless it's first one
+        mChapterListView.scrollTo(0, Math.max(0, item_size*(position-3)));
     }
 
     private void addChapters(List<Chapter> chapterList) {
-        Collections.sort(chapterList, mLocationComparator);
-        mSpinnerAdapter.clear();
-        mSpinnerAdapter.addAll(chapterList);
+        // For searching GDG Home city it's better to sort list basing on names rather then location
+        // TODO: Introduce CountryCity comparator to sort first by country and then by city
+        // TODO: this will allow to create list groups
+        Collections.sort(chapterList);
+        Object objArray[] = chapterList.toArray();
+        ArrayAdapter<Object> a = new ArrayAdapter<Object>(FirstStartStep1Fragment.this.getActivity(),
+                android.R.layout.simple_list_item_1, objArray);
+        mChapterListView.setAdapter(a);
 
-        mChapterSpinner.setAdapter(mSpinnerAdapter);
-
-        if(mSelectedChapter != null) {
-            int pos = mSpinnerAdapter.getPosition(mSelectedChapter);
-            mChapterSpinner.setSelection(pos);
+        int position = findSelectedChapterPosition();
+        if (position != -1) {
+            mChapterListView.requestFocusFromTouch();
+            mChapterListView.setSelection(position);
+            scrollToSelectedChapter(position);
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_welcome_step1, null);
-    }
-
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
     }
 
     public interface Step1Listener {
