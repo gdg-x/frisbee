b83e40a5c469c12b63700f1f2965394161a6358a
Friedger Müffke <friedger@gmail.com>
2014-12-18 23:17:40 +0100

    add toast if api keys are missing, fixes #157

--- app/src/main/java/org/gdg/frisbee/android/activity/StartActivity.java
+++ app/src/main/java/org/gdg/frisbee/android/activity/StartActivity.java
@@ -1,11 +1,16 @@
 package org.gdg.frisbee.android.activity;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.text.TextUtils;
+import android.widget.Toast;
 
+import org.gdg.frisbee.android.BuildConfig;
 import org.gdg.frisbee.android.Const;
+import org.gdg.frisbee.android.R;
 
 /**
  * Created with IntelliJ IDEA.
@@ -19,6 +24,10 @@ public class StartActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (BuildConfig.DEBUG && TextUtils.isEmpty(getString(R.string.play_app_id))){
+            Toast.makeText(this, "no API keys defined!", Toast.LENGTH_SHORT).show();
+        }
 
         SharedPreferences mPreferences = getSharedPreferences("gdg", MODE_PRIVATE);
 
