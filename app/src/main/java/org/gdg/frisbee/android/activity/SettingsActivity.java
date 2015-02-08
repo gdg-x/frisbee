a0cd19c8a7dda3a5050cd0d522f5a0e2e5b2be36
Henrique Rocha <hmrocha@gmail.com>
2015-02-08 10:31:01 +0100

    style: reformat code SettingsActivity
     Also update copyright and remove useless Javadoc.

--- app/src/main/java/org/gdg/frisbee/android/activity/SettingsActivity.java
+++ app/src/main/java/org/gdg/frisbee/android/activity/SettingsActivity.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 The GDG Frisbee Project
+ * Copyright 2013-2015 The GDG Frisbee Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,9 +16,7 @@
 
 package org.gdg.frisbee.android.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.view.MenuItem;
 
 import com.google.android.gms.analytics.HitBuilders;
@@ -27,29 +25,19 @@ import com.google.android.gms.analytics.Tracker;
 import org.gdg.frisbee.android.R;
 import org.gdg.frisbee.android.app.App;
 
-/**
- * GDG Aachen
- * org.gdg.frisbee.android.activity
- * <p/>
- * User: maui
- * Date: 25.06.13
- * Time: 21:15
- */
-public class SettingsActivity extends GdgActivity  {
-
+public class SettingsActivity extends GdgActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if(getSupportActionBar() != null) {
+        if (getSupportActionBar() != null) {
             getSupportActionBar().setTitle(R.string.settings);
             getSupportActionBar().setDisplayShowTitleEnabled(true);
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
 
         setContentView(R.layout.activity_settings);
-
 
         Tracker t = App.getInstance().getTracker();
         // Set screen name.
@@ -61,22 +49,15 @@ public class SettingsActivity extends GdgActivity  {
 
     }
 
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
-        if(item.getItemId() == android.R.id.home) {
+        if (item.getItemId() == android.R.id.home) {
             finish();
             return true;
         }
 
-        return super.onOptionsItemSelected(item);    //To change body of overridden methods use File | Settings | File Templates.
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int responseCode, Intent intent) {
-        super.onActivityResult(requestCode, responseCode, intent);
-
+        return super.onOptionsItemSelected(item);
     }
 
 }
