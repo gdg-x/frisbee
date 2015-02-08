e0dcb5bd0adabce4a21648d2557997101c85d132
Henrique Rocha <hmrocha@gmail.com>
2015-02-08 09:32:51 +0100

    docs: remove useless Javadoc and add copyright

--- app/src/main/java/org/gdg/frisbee/android/activity/StartActivity.java
+++ app/src/main/java/org/gdg/frisbee/android/activity/StartActivity.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2013-2015 The GDG Frisbee Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.gdg.frisbee.android.activity;
 
 import android.content.Intent;
@@ -11,13 +27,6 @@ import org.gdg.frisbee.android.BuildConfig;
 import org.gdg.frisbee.android.Const;
 import org.gdg.frisbee.android.R;
 
-/**
- * Created with IntelliJ IDEA.
- * User: maui
- * Date: 02.07.13
- * Time: 00:03
- * To change this template use File | Settings | File Templates.
- */
 public class StartActivity extends ActionBarActivity {
 
     @Override
@@ -40,4 +49,5 @@ public class StartActivity extends ActionBarActivity {
         startActivity(intentForStart);
         finish();
     }
+
 }
