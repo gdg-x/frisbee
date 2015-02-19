3ac74d468d6d4071a30eaa3c9d0e643fda44c077
Henrique Rocha <hmrocha@gmail.com>
2015-02-19 16:37:22 +0000

    test: test that the AboutActivity starts properly

---
+++ app/src/androidTest/java/org/gdg/frisbee/android/activity/AboutActivityTest.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2013-2015 The GDG Frisbee Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gdg.frisbee.android.activity;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import org.gdg.frisbee.android.R;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+public class AboutActivityTest extends ActivityInstrumentationTestCase2<AboutActivity> {
+
+    public AboutActivityTest() {
+        super(AboutActivity.class);
+    }
+
+    public void testAboutActivityStarts() {
+        getActivity();
+
+        onView(withText(R.string.about)).check(matches(isDisplayed()));
+    }
+}
