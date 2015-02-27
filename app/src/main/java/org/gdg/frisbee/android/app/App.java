3dc19a0cadf27b4dc771440bae590c5cab87f5a9
Said Tahsin Dane <tasomaniac@gmail.com>
2015-02-27 10:10:46 +0200

    Colors are added for special events

--- app/src/main/java/org/gdg/frisbee/android/app/App.java
+++ app/src/main/java/org/gdg/frisbee/android/app/App.java
@@ -145,6 +145,7 @@ public class App extends Application implements LocationListener {
                 R.string.wtm,
                 R.string.wtm_description,
                 R.drawable.ic_wtm,
+                R.color.color_wtm,
                 Const.START_TIME_WTM,
                 Const.END_TIME_WTM));
         //Add Android Fundamentals Study Jams
@@ -153,6 +154,7 @@ public class App extends Application implements LocationListener {
                 R.string.studyjams,
                 R.string.studyjams_description,
                 R.drawable.ic_studyjams,
+                R.color.color_studyjams,
                 Const.START_TIME_STUDY_JAMS,
                 Const.END_TIME_STUDY_JAMS));
         //Add IO Extended
@@ -161,6 +163,7 @@ public class App extends Application implements LocationListener {
                 R.string.ioextended,
                 R.string.ioextended_description,
                 R.drawable.ic_ioextended,
+                R.color.color_ioextended,
                 Const.START_TIME_IOEXTENDED,
                 Const.END_TIME_IOEXTENDED));
     }
