f6de662dd29c845ac3197ee00e387bf1d698ce15
Friedger Müffke <friedger@gmail.com>
2015-03-03 23:14:51 +0100

    disable list preference until list is loaded

--- app/src/main/java/org/gdg/frisbee/android/fragment/SettingsFragment.java
+++ app/src/main/java/org/gdg/frisbee/android/fragment/SettingsFragment.java
@@ -204,8 +204,7 @@ public class SettingsFragment extends PreferenceFragment {
     private void initPreferences() {
         final ListPreference prefHomeGdgList = (ListPreference) findPreference(PrefUtils.SETTINGS_HOME_GDG);
         if (prefHomeGdgList != null) {
-            prefHomeGdgList.setEntries(new String[0]);
-            prefHomeGdgList.setEntryValues(new String[0]);
+            prefHomeGdgList.setEnabled(false);
 
             App.getInstance().getModelCache().getAsync("chapter_list_hub", false, new ModelCache.CacheListener() {
                 @Override
@@ -223,6 +222,7 @@ public class SettingsFragment extends PreferenceFragment {
                     }
                     prefHomeGdgList.setEntries(entries);
                     prefHomeGdgList.setEntryValues(entryValues);
+                    prefHomeGdgList.setEnabled(true);
                 }
 
                 @Override
