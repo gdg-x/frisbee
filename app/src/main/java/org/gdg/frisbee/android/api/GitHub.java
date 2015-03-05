a77b1ca62fd93216e4ce418266c679657e1b5063
Said Tahsin Dane <tahsin@vngrs.com>
2015-03-05 17:45:03 +0200

    refactor: GitHub request now uses Retrofit

--- app/src/main/java/org/gdg/frisbee/android/api/GitHub.java
+++ app/src/main/java/org/gdg/frisbee/android/api/GitHub.java
@@ -16,43 +16,21 @@
 
 package org.gdg.frisbee.android.api;
 
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.google.gson.FieldNamingPolicy;
-import com.google.gson.reflect.TypeToken;
-
 import org.gdg.frisbee.android.api.model.Contributor;
 
-import java.lang.reflect.Type;
 import java.util.ArrayList;
 
-/**
- * Created with IntelliJ IDEA.
- * User: maui
- * Date: 08.07.13
- * Time: 02:08
- * To change this template use File | Settings | File Templates.
- */
-public class GitHub {
+import retrofit.Callback;
+import retrofit.http.GET;
+import retrofit.http.Path;
 
-    private static final String BASE_URL = " https://api.github.com";
-    private static final String CONTRIBUTORS_URL = BASE_URL + "/repos/%s/%s/contributors";
+public interface GitHub {
 
-    public ApiRequest getContributors(String user, String repo, 
-                                      Response.Listener<ArrayList<Contributor>> successListener, Response.ErrorListener errorListener) {
+    @GET("/repos/{owner}/{repo}/contributors")
+    void getContributors(@Path("owner") String user, 
+                         @Path("repo") String repo,
+                         Callback<ArrayList<Contributor>> callback);
 
-        Type type = new TypeToken<ArrayList<Contributor>>() {
-            }.getType();
-
-        String url = String.format(CONTRIBUTORS_URL, user, repo);
-
-        GsonRequest<Void, ArrayList<Contributor>> eventReq = new GsonRequest<Void, ArrayList<Contributor>>(Request.Method.GET,
-                url,
-                type,
-                successListener,
-                errorListener,
-                GsonRequest.getGson(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES));
-
-        return new ApiRequest(eventReq);
-    }
+//    @GET("/users/{username}")
+//    void getUserDetail(@Path("username") String username, Callback<EventFullDetails> callback);
 }
