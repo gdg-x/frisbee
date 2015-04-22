8830c1e9cb4960c491fcc9a6d53e252fb872cb64
Said Tahsin Dane <tahsin@vngrs.com>
2015-04-22 18:07:06 +0300

    replace: normal Dialog with AlertDialog Doorbel Dialog should be updated as well. But it is almost impossible.

--- app/src/main/java/org/gdg/frisbee/android/fragment/SeasonsGreetingsFragment.java
+++ app/src/main/java/org/gdg/frisbee/android/fragment/SeasonsGreetingsFragment.java
@@ -16,12 +16,12 @@
 
 package org.gdg.frisbee.android.fragment;
 
-import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
