0d75cb9549566a73539d0c7431ee655b9981909f
Friedger Müffke <friedger@gmail.com>
2015-04-30 12:26:30 +0200

    remove unused imports

--- app/src/main/java/org/gdg/frisbee/android/utils/Utils.java
+++ app/src/main/java/org/gdg/frisbee/android/utils/Utils.java
@@ -49,7 +49,6 @@ import java.net.URL;
 import java.net.URLDecoder;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import timber.log.Timber;
