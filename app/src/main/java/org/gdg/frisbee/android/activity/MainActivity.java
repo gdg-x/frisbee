67bb34913bcfba0c3a931894857ddf157a1f87c8
Said Tahsin Dane <tahsin@vngrs.com>
2015-05-05 14:21:40 +0300

    Organizer check is added to every onResume function. It is to update the tabs after coming from the settings screen after a logout.

--- app/src/main/java/org/gdg/frisbee/android/activity/MainActivity.java
+++ app/src/main/java/org/gdg/frisbee/android/activity/MainActivity.java
@@ -176,17 +176,23 @@ public class MainActivity extends GdgNavDrawerActivity {
             GooglePlayServicesUtil.getErrorDialog(playServiceStatus, this, PLAY_SERVICE_DIALOG_REQUEST_CODE).show();
         }
         checkHomeChapterValid();
+        checkOrganizer();
     }
 
     @Override
     public void onConnected(Bundle bundle) {
         super.onConnected(bundle);
+        checkOrganizer();
+    }
 
+    private void checkOrganizer() {
         App.getInstance().checkOrganizer(getGoogleApiClient(),
                 new OrganizerChecker.OrganizerResponseHandler() {
                     @Override
                     public void onOrganizerResponse(boolean isOrganizer) {
-                        mViewPagerAdapter.notifyDataSetChanged();
+                        if (mViewPagerAdapter != null) {
+                            mViewPagerAdapter.notifyDataSetChanged();
+                        }
                     }
 
                     @Override
