e9c5e1510a55be201104d917b1726bab3730af5c
Henrique Rocha <hmrocha@gmail.com>
2015-05-20 14:14:26 +0200

    Convert field to local variable

--- app/src/main/java/org/gdg/frisbee/android/widget/SlidingTabStrip.java
+++ app/src/main/java/org/gdg/frisbee/android/widget/SlidingTabStrip.java
@@ -38,8 +38,6 @@ class SlidingTabStrip extends LinearLayout {
     private final int mSelectedIndicatorThickness;
     private final Paint mSelectedIndicatorPaint;
 
-    private final int mDefaultBottomBorderColor;
-
     private int mSelectedPosition;
     private float mSelectionOffset;
 
@@ -60,7 +58,7 @@ class SlidingTabStrip extends LinearLayout {
         context.getTheme().resolveAttribute(android.R.attr.colorForeground, outValue, true);
         final int themeForegroundColor = outValue.data;
 
-        mDefaultBottomBorderColor = setColorAlpha(themeForegroundColor,
+        int mDefaultBottomBorderColor = setColorAlpha(themeForegroundColor,
                 DEFAULT_BOTTOM_BORDER_COLOR_ALPHA);
 
         mDefaultTabColorizer = new SimpleTabColorizer();
