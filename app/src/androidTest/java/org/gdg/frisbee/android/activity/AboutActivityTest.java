0ad46decf123c5ca612b512e582a222ed5f65859
Friedger Müffke <friedger@gmail.com>
2015-07-25 11:40:58 +0200

    move to junit4 tests

--- app/src/androidTest/java/org/gdg/frisbee/android/activity/AboutActivityTest.java
+++ app/src/androidTest/java/org/gdg/frisbee/android/activity/AboutActivityTest.java
@@ -16,25 +16,27 @@
 
 package org.gdg.frisbee.android.activity;
 
-import android.test.ActivityInstrumentationTestCase2;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
 
 import org.gdg.frisbee.android.R;
 import org.gdg.frisbee.android.about.AboutActivity;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
-public class AboutActivityTest extends ActivityInstrumentationTestCase2<AboutActivity> {
+@RunWith(AndroidJUnit4.class)
+public class AboutActivityTest {
+    @Rule
+    ActivityTestRule<AboutActivity> rule = new ActivityTestRule<>(AboutActivity.class);
 
-    public AboutActivityTest() {
-        super(AboutActivity.class);
-    }
-
-    public void testAboutActivityStarts() {
-        getActivity();
-
+    @Test
+    public void activityStarts() {
         onView(withText(R.string.about)).check(matches(isDisplayed()));
     }
 }
--- app/src/androidTest/java/org/gdg/frisbee/android/activity/FirstStartActivityTest.java
+++ app/src/androidTest/java/org/gdg/frisbee/android/activity/FirstStartActivityTest.java
@@ -1,9 +1,12 @@
 package org.gdg.frisbee.android.activity;
 
-import android.test.ActivityInstrumentationTestCase2;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
 
 import org.gdg.frisbee.android.R;
 import org.gdg.frisbee.android.onboarding.FirstStartActivity;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
@@ -11,14 +14,12 @@ import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
-public class FirstActivityTest extends ActivityInstrumentationTestCase2<FirstStartActivity> {
-
-    public FirstActivityTest() {
-        super(FirstStartActivity.class);
-    }
+@RunWith(AndroidJUnit4.class)
+public class FirstStartActivityTest {
+    @Rule
+    ActivityTestRule<FirstStartActivity> rule = new ActivityTestRule<>(FirstStartActivity.class);
 
     public void testFirstStartStep1ToStep2() {
-        getActivity();
         onView(withId(R.id.confirm)).perform(click());
         onView(withId(R.id.skipSignin)).check(matches(isDisplayed()));
     }
