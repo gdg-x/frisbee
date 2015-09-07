1a721ea4260e98c77c9454b8c1d347fb90e8d42b
Said Tahsin Dane <tasomaniac@gmail.com>
2015-09-07 14:36:56 +0300

    OkHttp interceptor is added for logging on Debug builds.

--- app/src/main/java/org/gdg/frisbee/android/api/OkClientFactory.java
+++ app/src/main/java/org/gdg/frisbee/android/api/OkClientFactory.java
@@ -4,9 +4,17 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 
 import com.squareup.okhttp.Cache;
+import com.squareup.okhttp.Interceptor;
 import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import org.gdg.frisbee.android.BuildConfig;
 
 import java.io.File;
+import java.io.IOException;
+
+import timber.log.Timber;
 
 public final class OkClientFactory {
 
@@ -23,6 +31,28 @@ public final class OkClientFactory {
         File cacheDir = new File(context.getApplicationContext().getCacheDir(), "http");
         Cache cache = new Cache(cacheDir, DISK_CACHE_SIZE);
         okHttpClient.setCache(cache);
+        if (BuildConfig.DEBUG) {
+            okHttpClient.interceptors().add(new LoggingInterceptor());
+        }
         return okHttpClient;
+    }
+
+    private static class LoggingInterceptor implements Interceptor {
+        @Override
+        public Response intercept(Chain chain) throws IOException {
+            Request request = chain.request();
+
+            long t1 = System.nanoTime();
+            Timber.i(String.format("Sending request %s on %s%n%s",
+                    request.url(), chain.connection(), request.headers()));
+
+            Response response = chain.proceed(request);
+
+            long t2 = System.nanoTime();
+            Timber.i(String.format("Received response for %s in %.1fms%n%s",
+                    response.request().url(), (t2 - t1) / 1e6d, response.headers()));
+
+            return response;
+        }
     }
 }
