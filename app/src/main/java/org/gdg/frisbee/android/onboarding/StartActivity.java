4689d57363b83394d3889f9f3a7a47a7983b4a9b
Friedger Müffke <friedger@gmail.com>
2015-11-24 08:53:21 +0100

    fix app invite

--- app/src/main/java/org/gdg/frisbee/android/onboarding/StartActivity.java
+++ app/src/main/java/org/gdg/frisbee/android/onboarding/StartActivity.java
@@ -42,7 +42,7 @@ public class StartActivity extends Activity {
         } else {
             intentForStart = new Intent(StartActivity.this, MainActivity.class);
         }
-
+        intentForStart.putExtras(getIntent().getExtras());
         startActivity(intentForStart);
         finish();
     }
