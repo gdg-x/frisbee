848bf9124bd5a1c3eb8322b55117ffd8093d255a
Friedger Müffke <friedger@gmail.com>
2015-12-15 22:22:10 +0100

    add filter to settings for GDG home

---
+++ app/src/main/java/org/gdg/frisbee/android/view/LocationListPreference.java
@@ -0,0 +1,262 @@
+package org.gdg.frisbee.android.view;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.preference.DialogPreference;
+import android.preference.PreferenceManager;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import org.gdg.frisbee.android.R;
+
+public class LocationListPreference extends DialogPreference {
+
+    private EditText editText;
+    private ListView listView;
+    private int clickedItemIndex;
+    private String mValue;
+    private boolean mValueSet;
+    private String[] mEntries;
+    private String[] mEntryValues;
+
+    public LocationListPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setDialogLayoutResource(R.layout.view_location_list_preference);
+    }
+
+    @Override
+    protected void onPrepareDialogBuilder(AlertDialog.Builder builder) {
+        super.onPrepareDialogBuilder(builder);
+        builder.setPositiveButton(null, null);
+    }
+
+    @Override
+    protected void onBindDialogView(View view) {
+        super.onBindDialogView(view);
+        editText = (EditText) view.findViewById(R.id.filter);
+        listView = (ListView) view.findViewById(android.R.id.list);
+        clickedItemIndex = findIndexOfValue(mValue);
+        listView.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        clickedItemIndex = position;
+                        LocationListPreference.this.onClick(getDialog(), DialogInterface.BUTTON_POSITIVE);
+                        getDialog().dismiss();
+                    }
+                }
+        );
+        listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        CheckedItemAdapter adapter = new CheckedItemAdapter(
+                getContext(), android.R.layout.simple_list_item_single_choice,
+                android.R.id.text1, getEntries()
+        );
+        listView.setAdapter(adapter);
+        clickedItemIndex = findIndexOfValue(getPersistedString(null));
+        listView.setSelection(clickedItemIndex);
+        listView.setItemChecked(clickedItemIndex, true);
+        listView.setTextFilterEnabled(true);
+
+        editText.addTextChangedListener(
+                new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                    }
+
+                    @Override
+                    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                    }
+
+                    @Override
+                    public void afterTextChanged(Editable editable) {
+                        listView.setFilterText(editable.toString());
+                    }
+                }
+        );
+        editText.requestFocus();
+        InputMethodManager imm = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.showSoftInput(editText, InputMethodManager.SHOW_IMPLICIT);
+    }
+
+    @Override
+    protected void onDialogClosed(boolean positiveResult) {
+        if (positiveResult && clickedItemIndex >= 0 && mEntryValues != null) {
+            String value = mEntryValues[clickedItemIndex].toString();
+            if (callChangeListener(value)) {
+                setValue(value);
+            }
+        }
+    }
+
+    public void setEntries(String[] entries) {
+        mEntries = entries;
+    }
+
+    public String[] getEntries() {
+        return mEntries;
+    }
+
+    public void setEntryValues(String[] entryValues) {
+        mEntryValues = entryValues;
+    }
+
+    public String[] getEntryValues() {
+        return mEntryValues;
+    }
+
+    public String getValue() {
+        return mValue;
+    }
+
+    public void setValue(String value) {
+        // Always persist/notify the first time.
+        final boolean changed = !TextUtils.equals(mValue, value);
+        if (changed || !mValueSet) {
+            mValue = value;
+            mValueSet = true;
+            persistString(value);
+            if (changed) {
+                notifyChanged();
+            }
+        }
+    }
+
+    /**
+     * Returns the index of the given value (in the entry values array).
+     *
+     * @param value The value whose index should be returned.
+     * @return The index of the value, or -1 if not found.
+     */
+    public int findIndexOfValue(String value) {
+        if (value != null && mEntryValues != null) {
+            for (int i = mEntryValues.length - 1; i >= 0; i--) {
+                if (mEntryValues[i].equals(value)) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public CharSequence getSummary() {
+        String persistedString = getPersistedString(null);
+        if (persistedString == null) {
+            return super.getSummary();
+        } else {
+            int index = findIndexOfValue(persistedString);
+            if (index >= 0) {
+                return mEntries[index];
+            } else {
+                return super.getSummary();
+            }
+        }
+    }
+
+    @Override
+    protected void onAttachedToHierarchy(PreferenceManager preferenceManager) {
+        super.onAttachedToHierarchy(preferenceManager);
+    }
+
+    @Override
+    protected void onSetInitialValue(boolean restoreValue, Object defaultValue) {
+        setValue(restoreValue ? getPersistedString(mValue) : (String) defaultValue);
+    }
+
+    @Override
+    protected Parcelable onSaveInstanceState() {
+        final Parcelable superState = super.onSaveInstanceState();
+
+        final SavedState myState = new SavedState(superState);
+        myState.value = getValue();
+        myState.entries = getEntries();
+        myState.entryValues = getEntryValues();
+        return myState;
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Parcelable state) {
+        if (state == null || !state.getClass().equals(SavedState.class)) {
+            // Didn't save state for us in onSaveInstanceState
+            super.onRestoreInstanceState(state);
+            return;
+        }
+
+        SavedState myState = (SavedState) state;
+        setEntries(myState.entries);
+        setEntryValues(myState.entryValues);
+        super.onRestoreInstanceState(myState.getSuperState());
+        setValue(myState.value);
+    }
+
+    private static class CheckedItemAdapter extends ArrayAdapter<CharSequence> {
+        public CheckedItemAdapter(Context context, int resource, int textViewResourceId,
+                                  CharSequence[] objects) {
+            super(context, resource, textViewResourceId, objects);
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+    }
+
+    private static class SavedState extends BaseSavedState {
+        int entriesSize;
+        String value;
+        String[] entries;
+        String[] entryValues;
+
+        public SavedState(Parcel source) {
+            super(source);
+            value = source.readString();
+            entriesSize = source.readInt();
+            entries = new String[entriesSize];
+            source.readStringArray(entries);
+            entryValues = new String[entriesSize];
+            source.readStringArray(entryValues);
+        }
+
+        @Override
+        public void writeToParcel(Parcel dest, int flags) {
+            super.writeToParcel(dest, flags);
+            dest.writeString(value);
+            dest.writeInt(entries.length);
+            dest.writeStringArray(entries);
+            dest.writeStringArray(entryValues);
+        }
+
+        public SavedState(Parcelable superState) {
+            super(superState);
+        }
+
+        public static final Parcelable.Creator<SavedState> CREATOR =
+            new Parcelable.Creator<SavedState>() {
+                public SavedState createFromParcel(Parcel in) {
+                    return new SavedState(in);
+                }
+
+                public SavedState[] newArray(int size) {
+                    return new SavedState[size];
+                }
+            };
+    }
+}
