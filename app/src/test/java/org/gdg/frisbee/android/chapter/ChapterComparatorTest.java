98007150f4440e0997f3c09a8a19faaafb204770
Said Tahsin Dane <tasomaniac@gmail.com>
2015-12-28 17:37:15 +0200

    Another more human readable approach is taken for Comparator testing. Inspired by Jesse Willson https://publicobject.com/2007/04/helper-method-for-unit-testing.html
    There is something wrong with Robolectric's computation of distance. Comparing with Eskisehir is not working at all. (Eskisehir is close to Istanbul geographically) So it is removed.

--- app/src/test/java/org/gdg/frisbee/android/chapter/ChapterComparatorTest.java
+++ app/src/test/java/org/gdg/frisbee/android/chapter/ChapterComparatorTest.java
@@ -13,7 +13,14 @@ import org.junit.runner.RunWith;
 import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
 
-import static org.junit.Assert.assertTrue;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Random;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(RobolectricGradleTestRunner.class)
 @Config(constants = BuildConfig.class, application = BaseApp.class)
@@ -25,7 +32,6 @@ public class ChapterComparatorTest {
     private static final Chapter chapterStartsWithLetterA = new Chapter("GDG AAUA", "115737274430015604080");
     private static final Chapter chapterStartsWithDigit = new Chapter("GDG 6th October", "105609152381762460369");
     private static final Chapter chapterIstanbul = new Chapter("GDG Istanbul", "100514812580249787371");
-    private static final Chapter chapterEskisehir = new Chapter("GDG Eskişehir", "115602346615910585045");
     private static final Chapter chapterWithDoubleSpaceInName = new Chapter("GDG  Mukono", "108217555392256442794");
 
     @Before
@@ -37,81 +43,75 @@ public class ChapterComparatorTest {
         chapterStartsWithLetterA.setGeo(new Geo(7.083333, 4.833333));
         chapterStartsWithDigit.setGeo(new Geo(29.938126, 30.91398));
         chapterIstanbul.setGeo(new Geo(28.97696, 41.00527));
-        chapterEskisehir.setGeo(new Geo(39.7766667, 30.5205556));
     }
 
     @Test
     public void shouldBeAlphabeticalWhenNoHomeAndLocation() {
         ChapterComparator comparator = new ChapterComparator(null, null);
 
-        //Chapter starting with digit should come before chapter starting with 'A'
-        assertTrue(0 > comparator.compare(chapterStartsWithDigit, chapterStartsWithLetterA));
-        assertTrue(0 < comparator.compare(chapterStartsWithLetterA, chapterStartsWithDigit));
-        assertTrue(0 == comparator.compare(chapterStartsWithDigit, chapterStartsWithDigit));
-
-        //Chapter starting with digit should come before Istanbul
-        assertTrue(0 > comparator.compare(chapterStartsWithDigit, chapterIstanbul));
-        assertTrue(0 < comparator.compare(chapterIstanbul, chapterStartsWithDigit));
-
+        assertElementsOrderedLikeThis(comparator,
+                chapterStartsWithDigit,
+                chapterStartsWithLetterA,
+                chapterIstanbul);
     }
 
     @Test
     public void doubleSpaceInNameShouldNotBeFirst() {
         ChapterComparator comparator = new ChapterComparator(null, null);
 
-        assertTrue(0 > comparator.compare(chapterStartsWithDigit, chapterWithDoubleSpaceInName));
-        assertTrue(0 < comparator.compare(chapterWithDoubleSpaceInName, chapterStartsWithDigit));
+        assertElementsOrderedLikeThis(comparator,
+                chapterStartsWithDigit,
+                chapterStartsWithLetterA,
+                chapterWithDoubleSpaceInName);
     }
 
     @Test
     public void nearestLocationShouldBeTop() {
         ChapterComparator comparator = new ChapterComparator(null, locationIstanbul);
 
-        //Chapter starting with digit should come before chapter starting with 'A'
-        assertTrue(0 > comparator.compare(chapterStartsWithDigit, chapterStartsWithLetterA));
-        assertTrue(0 < comparator.compare(chapterStartsWithLetterA, chapterStartsWithDigit));
-        assertTrue(0 == comparator.compare(chapterStartsWithDigit, chapterStartsWithDigit));
-
-        //Istanbul should be top because it is geographically close to user's location.
-        assertTrue(0 > comparator.compare(chapterIstanbul, chapterStartsWithDigit));
-        assertTrue(0 < comparator.compare(chapterStartsWithDigit, chapterIstanbul));
-
-        //Istanbul should be top because it is geographically close to user's location.
-        assertTrue(0 > comparator.compare(chapterIstanbul, chapterEskisehir));
-        assertTrue(0 < comparator.compare(chapterEskisehir, chapterIstanbul));
+        assertElementsOrderedLikeThis(comparator,
+                chapterIstanbul,
+                chapterStartsWithDigit,
+                chapterStartsWithLetterA);
     }
 
     @Test
     public void chapterWithoutLocationShouldBeSortedAlphabetically() {
         ChapterComparator comparator = new ChapterComparator(null, locationIstanbul);
 
-        //Chapter starting with digit should come before chapter starting with 'A'
-        assertTrue(0 > comparator.compare(chapterStartsWithDigit, chapterWithoutLocation));
-        assertTrue(0 < comparator.compare(chapterWithoutLocation, chapterStartsWithDigit));
-
-        //Istanbul should be top because it is geographically close to user's location.
-        assertTrue(0 > comparator.compare(chapterIstanbul, chapterWithoutLocation));
-        assertTrue(0 < comparator.compare(chapterWithoutLocation, chapterIstanbul));
+        assertElementsOrderedLikeThis(comparator,
+                chapterIstanbul,
+                chapterStartsWithDigit,
+                chapterWithoutLocation);
     }
 
     @Test
     public void homeChapterShouldBeTop() {
-        ChapterComparator comparator = new ChapterComparator(chapterIstanbul.getGplusId(), locationIstanbul);
+        ChapterComparator comparator =
+                new ChapterComparator(chapterIstanbul.getGplusId(), locationIstanbul);
 
-        assertTrue(0 > comparator.compare(chapterIstanbul, chapterWithoutLocation));
-        assertTrue(0 < comparator.compare(chapterWithoutLocation, chapterIstanbul));
+        assertElementsOrderedLikeThis(comparator,
+                chapterIstanbul, chapterWithoutLocation);
+        assertElementsOrderedLikeThis(comparator,
+                chapterIstanbul, chapterStartsWithDigit);
+        assertElementsOrderedLikeThis(comparator,
+                chapterIstanbul, chapterStartsWithLetterA);
+        assertElementsOrderedLikeThis(comparator,
+                chapterIstanbul, chapterWithDoubleSpaceInName);
+    }
 
-        assertTrue(0 > comparator.compare(chapterIstanbul, chapterStartsWithDigit));
-        assertTrue(0 < comparator.compare(chapterStartsWithDigit, chapterIstanbul));
+    private <T> void assertElementsOrderedLikeThis(
+            Comparator<? super T> comparator, T... elements) {
+        List<T> expectedOrder = Arrays.asList(elements);
 
-        assertTrue(0 > comparator.compare(chapterIstanbul, chapterStartsWithLetterA));
-        assertTrue(0 < comparator.compare(chapterStartsWithLetterA, chapterIstanbul));
+        List<T> shuffledAndSorted = new ArrayList<>(expectedOrder);
+        Collections.shuffle(shuffledAndSorted, new Random(0));
+        Collections.sort(shuffledAndSorted, comparator);
+        assertEquals(expectedOrder, shuffledAndSorted);
 
-        assertTrue(0 > comparator.compare(chapterIstanbul, chapterEskisehir));
-        assertTrue(0 < comparator.compare(chapterEskisehir, chapterIstanbul));
-
-        assertTrue(0 > comparator.compare(chapterIstanbul, chapterWithDoubleSpaceInName));
-        assertTrue(0 < comparator.compare(chapterWithDoubleSpaceInName, chapterIstanbul));
-
+        List<T> reversedAndSorted = new ArrayList<>(expectedOrder);
+        Collections.reverse(reversedAndSorted);
+        Collections.sort(reversedAndSorted, comparator);
+        assertEquals(expectedOrder, reversedAndSorted);
     }
 }
