e5bfb1b7c954329a8e1e9cf4e3d2c3919da54828
friedger <friedger@googlemail.com>
2016-01-19 12:49:08 +0100

    add error handler and add feedback on success

--- app/src/main/java/org/gdg/frisbee/android/widget/FeedbackFragment.java
+++ app/src/main/java/org/gdg/frisbee/android/widget/FeedbackFragment.java
@@ -33,6 +33,7 @@ import android.os.Build.VERSION;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
@@ -52,6 +53,7 @@ import org.gdg.frisbee.android.achievements.AchievementActionHandler;
 import org.gdg.frisbee.android.common.GdgActivity;
 import org.gdg.frisbee.android.utils.PrefUtils;
 import org.gdg.frisbee.android.utils.Utils;
+import org.gdg.frisbee.android.view.ColoredSnackBar;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -63,6 +65,7 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 import io.doorbell.android.DoorbellApi;
 import io.doorbell.android.manavo.rest.RestCallback;
+import io.doorbell.android.manavo.rest.RestErrorCallback;
 import timber.log.Timber;
 
 public class FeedbackFragment extends DialogFragment {
@@ -183,14 +186,22 @@ public class FeedbackFragment extends DialogFragment {
         mApi.setLoadingMessage(getActivity().getString(R.string.feedback_sending));
         mApi.setCallback(new RestCallback() {
             public void success(Object obj) {
-                //TODO add feedback
-                //Toast.makeText(getActivity(), obj.toString(), Toast.LENGTH_SHORT).show();
                 mMessageField.setText("");
                 mProperties = new JSONObject();
 
                 if (achievementHandler != null) {
                     achievementHandler.handleKissesFromGdgXTeam();
+                } else {
+                    Snackbar snackbar = Snackbar.make(getView(), R.string.thanks_for_feedback, Snackbar.LENGTH_LONG);
+                    ColoredSnackBar.info(snackbar).show();
                 }
+            }
+        });
+        mApi.setErrorCallback(new RestErrorCallback() {
+            @Override
+            public void error(String s) {
+                Snackbar snackbar = Snackbar.make(getView(), s, Snackbar.LENGTH_LONG);
+                ColoredSnackBar.alert(snackbar).show();
             }
         });
         mApi.sendFeedback(mMessageField.getText().toString(),
