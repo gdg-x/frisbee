cfd8dab3906a281649426f161c77f45267a9cc95
Said Tahsin Dane <tasomaniac@gmail.com>
2016-01-21 12:21:32 +0200

    Use methods in sh script.

--- .buildscript/deploy_alpha.sh
+++ .buildscript/deploy_alpha.sh
@@ -8,31 +8,21 @@
 SLUG="gdg-x/frisbee"
 BRANCH="master"
 
-if [ "$TRAVIS_REPO_SLUG" != "$SLUG" ]; then
-  echo "Skipping alpha deployment: wrong repository. Expected '$SLUG' but was '$TRAVIS_REPO_SLUG'."
-elif [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-  echo "Skipping alpha deployment: was pull request."
-elif [ "$TRAVIS_BRANCH" != "$BRANCH" ]; then
-  echo "Skipping alpha deployment: wrong branch. Expected '$BRANCH' but was '$TRAVIS_BRANCH'."
-else
-  #Check if the branch has a tag
-  #If we don't have a tag, increase the build number, push a commit with a tag and release it.
-  git describe --exact-match >/dev/null 2>/dev/null
-  if [ $? -eq 0 ]; then
-    echo "Skipping alpha deployment: commit already has a tag."
-    exit 0
-  fi
-
-  echo "Checking out branch: $BRANCH"
-  git checkout $BRANCH
+incrementVersion () {
   echo "Incrementing version number..."
   perl -pi -e 's/(versionBuild\s=\s)(\d+)/$1.($2+1)/ge' build.gradle
-  version=$(perl -ne 'print "$1." if /version(?:Major|Minor|Patch|Build)\s=\s(\d+)/' build.gradle | sed 's/\.$//')
-  echo "Automated alpha build from CI" > app/src/alpha/play/en-US/whatsnew
-  echo "Deploying alpha APK version $version"
-  ./gradlew clean publishApkProdAlpha -Dtrack=alpha
-  if [ $? -eq 0 ]; then
-    echo "Alpha APK successfully deployed!"
+}
+
+getVersionName () {
+  return $(perl -ne 'print "$1." if /version(?:Major|Minor|Patch|Build)\s=\s(\d+)/' build.gradle | sed 's/\.$//')
+}
+
+updateWhatsNewFile () {
+  echo -e "Automated alpha build from CI\n\nLatest git commit:\n" > app/src/alpha/play/en-US/whatsnew
+  git log -1 --oneline >> app/src/alpha/play/en-US/whatsnew
+}
+
+commitAndPushToGit () {
     git config user.email "GDG-X"
     git config user.name "support@gdgx.io"
     git add build.gradle
@@ -42,5 +32,33 @@ else
     # This works because the API key can replace your password.
     git push -q https://gdg-x:$GITHUB_API_KEY@github.com/gdg-x/frisbee 2>/dev/null
     git push -q --tags https://gdg-x:$GITHUB_API_KEY@github.com/gdg-x/frisbee 2>/dev/null
+}
+
+isAlreadyDeployed () {
+  #Check if the branch has a tag
+  #If we don't have a tag, increase the build number, push a commit with a tag and release it.
+  git describe --exact-match >/dev/null 2>/dev/null
+  [ $? -eq 0 ]
+}
+
+if [ "$TRAVIS_REPO_SLUG" != "$SLUG" ]; then
+  echo "Skipping alpha deployment: wrong repository. Expected '$SLUG' but was '$TRAVIS_REPO_SLUG'."
+elif [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  echo "Skipping alpha deployment: was pull request."
+elif [ "$TRAVIS_BRANCH" != "$BRANCH" ]; then
+  echo "Skipping alpha deployment: wrong branch. Expected '$BRANCH' but was '$TRAVIS_BRANCH'."
+elif isAlreadyDeployed; then
+  echo "Skipping alpha deployment: commit already has a tag."
+else
+  echo "Checking out branch: $BRANCH"
+  git checkout $BRANCH
+  incrementVersion
+  version=getVersionName
+  updateWhatsNewFile
+  echo "Deploying alpha APK version $version"
+  ./gradlew publishApkProdAlpha -Dtrack=alpha
+  if [ $? -eq 0 ]; then
+    echo "Alpha APK successfully deployed!"
+    commitAndPushToGit
   fi
 fi
