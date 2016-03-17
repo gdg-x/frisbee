211ae58d50c23a8b123b4b96cba33a43890ee77f
friedger <friedger@googlemail.com>
2016-03-17 15:14:42 +0100

    organize imports

--- app/src/debug/java/org/gdg/frisbee/android/app/BaseApp.java
+++ app/src/debug/java/org/gdg/frisbee/android/app/BaseApp.java
@@ -1,9 +1,6 @@
 package org.gdg.frisbee.android.app;
 
 import android.app.Application;
-import android.content.Context;
-import android.support.multidex.MultiDex;
-import android.support.multidex.MultiDexApplication;
 
 public class BaseApp extends Application {
 
