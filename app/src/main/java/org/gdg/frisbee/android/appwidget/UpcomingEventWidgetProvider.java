78d32ab301750a4826532a05ae044e27c8e7d158
Said Tahsin Dane <tahsin@novoda.com>
2016-05-31 13:13:35 +0200

    Little cleanup after the PR merge.

--- app/src/main/java/org/gdg/frisbee/android/appwidget/UpcomingEventWidgetProvider.java
+++ app/src/main/java/org/gdg/frisbee/android/appwidget/UpcomingEventWidgetProvider.java
@@ -126,22 +126,16 @@ public class UpcomingEventWidgetProvider extends AppWidgetProvider {
 
         @Nullable
         private Event getNextEvent(List<Event> listEvents) {
-            if (listEvents == null || listEvents.size() == 0) {
-                return null;
-            }
-            Event ret = null;
+            Event nextEvent = null;
             for (Event e : listEvents) {
-                if (e.getStart().isAfterNow()) {
-                    if (ret == null) {
-                        ret = e;
-                    } else {
-                        if (e.getStart().isBefore(ret.getStart())) {
-                            ret = e;
-                        }
-                    }
+                if (e.getStart().isBeforeNow()) {
+                    continue;
+                }
+                if (nextEvent == null || e.getStart().isBefore(nextEvent.getStart())) {
+                    nextEvent = e;
                 }
             }
-            return  ret;
+            return nextEvent;
         }
 
         private void fetchEvents(Chapter homeGdg,
@@ -155,27 +149,21 @@ public class UpcomingEventWidgetProvider extends AppWidgetProvider {
                 .enqueue(new Callback<PagedList<Event>>() {
                     @Override
                     public void success(PagedList<Event> eventsPagedList) {
-                        List<Event> events = eventsPagedList.getItems();
-                        Timber.d("Got events");
-                        if (events.size() > 0) {
-                            Event nextEvent = getNextEvent(events);
-                            if (nextEvent != null) {
-                                views.setTextViewText(R.id.title, nextEvent.getTitle());
-                                views.setTextViewText(R.id.location, nextEvent.getLocation());
-                                views.setTextViewText(R.id.startDate,
-                                    nextEvent.getStart().toLocalDateTime()
-                                        .toString(DateTimeFormat.patternForStyle("MS",
-                                            getResources().getConfiguration().locale)));
-                                showChild(views, 1);
+                        Event nextEvent = getNextEvent(eventsPagedList.getItems());
 
-                                Intent i = new Intent(UpdateService.this, EventActivity.class);
-                                i.putExtra(Const.EXTRA_EVENT_ID, nextEvent.getId());
-                                views.setOnClickPendingIntent(R.id.container,
-                                    PendingIntent.getActivity(UpdateService.this, 0, i, 0));
-                            } else {
-                                showErrorChild(views, R.string.no_scheduled_events, UpdateService.this);
-                            }
+                        if (nextEvent != null) {
+                            views.setTextViewText(R.id.title, nextEvent.getTitle());
+                            views.setTextViewText(R.id.location, nextEvent.getLocation());
+                            views.setTextViewText(R.id.startDate,
+                                nextEvent.getStart().toLocalDateTime()
+                                    .toString(DateTimeFormat.patternForStyle("MS",
+                                        getResources().getConfiguration().locale)));
+                            showChild(views, 1);
 
+                            Intent i = new Intent(UpdateService.this, EventActivity.class);
+                            i.putExtra(Const.EXTRA_EVENT_ID, nextEvent.getId());
+                            views.setOnClickPendingIntent(R.id.container,
+                                PendingIntent.getActivity(UpdateService.this, 0, i, 0));
                         } else {
                             showErrorChild(views, R.string.no_scheduled_events, UpdateService.this);
                         }
