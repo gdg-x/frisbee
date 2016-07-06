a359b87d495d92a40ab5e944c5bb60f0ca80b38b
Said Tahsin Dane <tasomaniac@gmail.com>
2016-07-07 00:14:45 +0200

    Fix Notification Test Put sleep to wait for notification panel open. Check title instead of description because description is not shown when there is no events.

--- app/src/androidTest/java/org/gdg/frisbee/android/activity/NotificationTest.java
+++ app/src/androidTest/java/org/gdg/frisbee/android/activity/NotificationTest.java
@@ -6,13 +6,14 @@ import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.test.uiautomator.By;
 import android.support.test.uiautomator.UiDevice;
-import android.support.test.uiautomator.UiObject2;
 import android.support.v4.app.NotificationManagerCompat;
+import android.support.v7.widget.Toolbar;
 
 import org.gdg.frisbee.android.app.App;
 import org.gdg.frisbee.android.chapter.MainActivity;
 import org.gdg.frisbee.android.eventseries.NotificationHandler;
 import org.gdg.frisbee.android.eventseries.TaggedEventSeries;
+import org.hamcrest.core.AllOf;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -20,7 +21,9 @@ import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
@@ -35,16 +38,25 @@ public class NotificationTest {
     }
 
     @Test
-    public void testNotificationForEventSeries() {
+    public void testNotificationForEventSeries() throws InterruptedException {
+        NotificationManagerCompat nm = NotificationManagerCompat.from(rule.getActivity());
+
         int i = 0;
         for (TaggedEventSeries series : App.getInstance().currentTaggedEventSeries()) {
-            Notification notification = new NotificationHandler(rule.getActivity(), series).createNotification();
-            NotificationManagerCompat.from(rule.getActivity()).notify(i, notification);
+            Notification notification = new NotificationHandler(rule.getActivity(), series)
+                .createNotification();
+            nm.notify(i, notification);
 
             mDevice.openNotification();
+            Thread.sleep(1000);
             mDevice.findObject(By.textContains(rule.getActivity().getString(series.getTitleResId())))
                 .click();
-            onView(withText(series.getDescriptionResId())).check(matches(isDisplayed()));
+            Thread.sleep(1000);
+            onView(
+                AllOf.allOf(
+                    withText(series.getTitleResId()),
+                    withParent(isAssignableFrom(Toolbar.class)))
+            ).check(matches(isDisplayed()));
         }
     }
 }
