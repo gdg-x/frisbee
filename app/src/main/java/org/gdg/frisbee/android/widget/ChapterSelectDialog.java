65123a496e3d7220d74f939ae799544bd22b05f5
Said Tahsin Dane <tahsin@novoda.com>
2016-07-14 19:55:59 +0300

    Added ChapterSelectDialog

--- app/src/main/java/org/gdg/frisbee/android/widget/ChapterSelectDialog.java
+++ app/src/main/java/org/gdg/frisbee/android/widget/ChapterSelectDialog.java
@@ -2,7 +2,6 @@ package org.gdg.frisbee.android.widget;
 
 import android.app.Dialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -18,21 +17,43 @@ import android.widget.Filter;
 import android.widget.ListView;
 
 import org.gdg.frisbee.android.R;
+import org.gdg.frisbee.android.api.model.Chapter;
 import org.gdg.frisbee.android.view.FilterListView;
-import org.gdg.frisbee.android.view.LocationListPreference;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class ChapterSelectDialog extends AppCompatDialogFragment
-    implements
-    AdapterView.OnItemClickListener {
+    implements AdapterView.OnItemClickListener {
 
-    private SearchView cityNameSearchView;
-    private FilterListView listView;
-    private int clickedItemIndex;
-    private String mValue;
-    private boolean mValueSet;
-    private String[] mEntries;
-    private String[] mEntryValues;
-    private Listener listener;
+    private static final String EXTRA_CHAPTERS = "EXTRA_CHAPTERS";
+
+    @BindView(R.id.filter) SearchView cityNameSearchView;
+    @BindView(android.R.id.list) FilterListView listView;
+
+    private List<Chapter> chapters;
+
+    private Listener listener = Listener.EMPTY;
+    private Chapter selectedChapter;
+
+    public static ChapterSelectDialog newInstance(ArrayList<Chapter> chapters) {
+        ChapterSelectDialog fragment = new ChapterSelectDialog();
+        Bundle args = new Bundle(1);
+        args.putParcelableArrayList(EXTRA_CHAPTERS, chapters);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        chapters = getArguments().getParcelableArrayList(EXTRA_CHAPTERS);
+        selectedChapter = chapters.get(0);
+    }
 
     @NonNull
     @Override
@@ -46,8 +67,7 @@ public class ChapterSelectDialog extends AppCompatDialogFragment
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.view_location_list_preference, container, false);
-        cityNameSearchView = (SearchView) view.findViewById(R.id.filter);
-        listView = (FilterListView) view.findViewById(android.R.id.list);
+        ButterKnife.bind(this, view);
         return view;
     }
 
@@ -55,22 +75,21 @@ public class ChapterSelectDialog extends AppCompatDialogFragment
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        clickedItemIndex = findIndexOfValue(mValue);
         listView.setOnItemClickListener(this);
 
         listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
         final CheckedItemAdapter adapter = new CheckedItemAdapter(
-            getContext(), android.R.layout.simple_list_item_single_choice,
-            android.R.id.text1, getEntries()
+            getContext(),
+            android.R.layout.simple_list_item_single_choice,
+            android.R.id.text1,
+            chapters
         );
         listView.setAdapter(adapter);
-        clickedItemIndex = findIndexOfValue(getPersistedString(null));
-        listView.setSelection(clickedItemIndex);
-        listView.setItemChecked(clickedItemIndex, true);
+        listView.setItemChecked(0, true); // First item is always the home chapter.
         final Filter.FilterListener filterListener = new Filter.FilterListener() {
             @Override
             public void onFilterComplete(int count) {
-                int index = findIndexByLabelInFilteredListView(getPersistedString(null));
+                int index = findIndexByValueInFilteredListView(selectedChapter);
                 listView.setItemChecked(index, true);
             }
         };
@@ -92,17 +111,18 @@ public class ChapterSelectDialog extends AppCompatDialogFragment
         cityNameSearchView.requestFocus();
     }
 
-    @Override
-    public void onDismiss(DialogInterface dialog) {
-        super.onDismiss(dialog);
-
-        if (clickedItemIndex >= 0 && mEntryValues != null) {
-            String value = mEntryValues[clickedItemIndex];
-            listView.setItemChecked(clickedItemIndex, true);
-            if (callChangeListener(value)) {
-                setValue(value);
+    private int findIndexByValueInFilteredListView(Chapter value) {
+        if (value == null || listView == null) {
+            return -1;
+        }
+        Adapter adapter = listView.getAdapter();
+        for (int i = adapter.getCount() - 1; i >= 0; i--) {
+            Chapter item = (Chapter) adapter.getItem(i);
+            if (item.equals(value)) {
+                return i;
             }
         }
+        return -1;
     }
 
     @Override
@@ -119,92 +139,18 @@ public class ChapterSelectDialog extends AppCompatDialogFragment
         super.onDetach();
     }
 
-    public String[] getEntries() {
-        return mEntries;
-    }
-
-    public void setEntries(String[] entries) {
-        mEntries = entries;
-    }
-
-    public String[] getEntryValues() {
-        return mEntryValues;
-    }
-
-    public void setEntryValues(String[] entryValues) {
-        mEntryValues = entryValues;
-    }
-
-    public String getValue() {
-        return mValue;
-    }
-
-    public void setValue(String value) {
-        // Always persist/notify the first time.
-        final boolean changed = !TextUtils.equals(mValue, value);
-        if (changed || !mValueSet) {
-            mValue = value;
-            mValueSet = true;
-            persistString(value);
-            if (changed) {
-                notifyChanged();
-            }
-        }
-    }
-    /**
-     * Returns the index of the given value (in the entry values array).
-     *
-     * @param value The value whose index should be returned.
-     * @return The index of the value, or -1 if not found.
-     */
-    public int findIndexOfValue(String value) {
-        if (value != null && mEntryValues != null) {
-            for (int i = mEntryValues.length - 1; i >= 0; i--) {
-                if (mEntryValues[i].equals(value)) {
-                    return i;
-                }
-            }
-        }
-        return -1;
-    }
-
-    private int findIndexByLabelInFilteredListView(String value) {
-        if (value != null && listView != null) {
-            Adapter adapter = listView.getAdapter();
-            for (int i = adapter.getCount() - 1; i >= 0; i--) {
-                String label = (String) adapter.getItem(i);
-                int index = findIndexByLabel(label);
-                if (mEntryValues[index].equals(value)) {
-                    return i;
-                }
-            }
-        }
-        return -1;
-    }
-
-    private int findIndexByLabel(String value) {
-        if (value != null && mEntries != null) {
-            for (int i = mEntries.length - 1; i >= 0; i--) {
-                if (mEntries[i].equals(value)) {
-                    return i;
-                }
-            }
-        }
-        return -1;
-    }
-
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         if (getDialog().isShowing()) {
-            clickedItemIndex = findIndexByLabel((String) parent.getItemAtPosition(position));
-            LocationListPreference.this.onClick(getDialog(), DialogInterface.BUTTON_POSITIVE);
+            selectedChapter = (Chapter) parent.getItemAtPosition(position);
+            listener.onChapterSelected(selectedChapter);
             getDialog().dismiss();
         }
     }
 
-    private static class CheckedItemAdapter extends ArrayAdapter<CharSequence> {
-        public CheckedItemAdapter(Context context, int resource, int textViewResourceId,
-                                  CharSequence[] objects) {
+    private static class CheckedItemAdapter extends ArrayAdapter<Chapter> {
+        CheckedItemAdapter(Context context, int resource, int textViewResourceId,
+                           List<Chapter> objects) {
             super(context, resource, textViewResourceId, objects);
         }
 
@@ -215,11 +161,11 @@ public class ChapterSelectDialog extends AppCompatDialogFragment
     }
 
     interface Listener {
-        void onChapterSelected(String chapterId);
+        void onChapterSelected(Chapter selectedChapter);
 
         Listener EMPTY = new Listener() {
             @Override
-            public void onChapterSelected(String chapterId) {
+            public void onChapterSelected(Chapter selectedChapter) {
                 // no-op
             }
         };
