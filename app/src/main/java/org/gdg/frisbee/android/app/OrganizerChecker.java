f788d163c0a398b34aea11977a76346cb6858262
Said Tahsin Dane <tasomaniac@gmail.com>
2016-09-17 21:58:17 +0200

    Move from hungarian notation

--- app/src/main/java/org/gdg/frisbee/android/app/OrganizerChecker.java
+++ app/src/main/java/org/gdg/frisbee/android/app/OrganizerChecker.java
@@ -17,37 +17,37 @@ public class OrganizerChecker {
     private static final String PREF_ORGANIZER_STATE = "pref_organizer_state";
     private static final long ORGANIZER_CHECK_MAX_TIME = DateUtils.WEEK_IN_MILLIS;
 
-    private boolean mIsOrganizer = false;
-    private long mLastOrganizerCheck = 0;
-    private String mCheckedId = null;
-    private SharedPreferences mPreferences;
+    private boolean isOrganizer = false;
+    private long lastOrganizerCheckTimeStamp = 0;
+    private String checkedId = null;
+    private SharedPreferences preferences;
 
     OrganizerChecker(SharedPreferences preferences) {
-        mPreferences = preferences;
+        this.preferences = preferences;
         initOrganizer();
     }
 
     private void initOrganizer() {
-        mLastOrganizerCheck = mPreferences.getLong(PREF_ORGANIZER_CHECK_TIME, 0);
-        mCheckedId = mPreferences.getString(PREF_ORGANIZER_CHECK_ID, null);
-        mIsOrganizer = mPreferences.getBoolean(PREF_ORGANIZER_STATE, false);
+        lastOrganizerCheckTimeStamp = preferences.getLong(PREF_ORGANIZER_CHECK_TIME, 0);
+        checkedId = preferences.getString(PREF_ORGANIZER_CHECK_ID, null);
+        isOrganizer = preferences.getBoolean(PREF_ORGANIZER_STATE, false);
     }
 
     void resetOrganizer() {
-        mPreferences.edit()
+        preferences.edit()
             .putBoolean(OrganizerChecker.PREF_ORGANIZER_STATE, false)
             .putLong(OrganizerChecker.PREF_ORGANIZER_CHECK_TIME, 0)
             .apply();
     }
 
     public boolean isOrganizer() {
-        return mIsOrganizer;
+        return isOrganizer;
     }
 
     void checkOrganizer(GoogleApiClient apiClient, final Callbacks responseHandler) {
         if (!PrefUtils.isSignedIn(apiClient.getContext())) {
-            mIsOrganizer = false;
-            mCheckedId = null;
+            isOrganizer = false;
+            checkedId = null;
             responseHandler.onOrganizerResponse(false);
             return;
         }
@@ -55,35 +55,35 @@ public class OrganizerChecker {
         final String currentId = PlusUtils.getCurrentPersonId(apiClient);
 
         if (currentId != null
-            && (!currentId.equals(mCheckedId)
-            || System.currentTimeMillis() > mLastOrganizerCheck + ORGANIZER_CHECK_MAX_TIME)) {
-            mIsOrganizer = false;
+            && (!currentId.equals(checkedId)
+            || System.currentTimeMillis() > lastOrganizerCheckTimeStamp + ORGANIZER_CHECK_MAX_TIME)) {
+            isOrganizer = false;
             App.getInstance().getGdgXHub().checkOrganizer(currentId).enqueue(new Callback<OrganizerCheckResponse>() {
                 @Override
                 public void success(OrganizerCheckResponse organizerCheckResponse) {
-                    mLastOrganizerCheck = System.currentTimeMillis();
-                    mCheckedId = currentId;
-                    mIsOrganizer = organizerCheckResponse.getChapters().size() > 0;
-                    responseHandler.onOrganizerResponse(mIsOrganizer);
+                    lastOrganizerCheckTimeStamp = System.currentTimeMillis();
+                    checkedId = currentId;
+                    isOrganizer = organizerCheckResponse.getChapters().size() > 0;
+                    responseHandler.onOrganizerResponse(isOrganizer);
 
                     savePreferences();
                 }
 
                 @Override
                 public void failure(Throwable error) {
-                    mIsOrganizer = false;
+                    isOrganizer = false;
                     responseHandler.onErrorResponse();
                 }
             });
         } else {
-            responseHandler.onOrganizerResponse(mIsOrganizer);
+            responseHandler.onOrganizerResponse(isOrganizer);
         }
     }
 
     private void savePreferences() {
-        mPreferences.edit()
-            .putLong(PREF_ORGANIZER_CHECK_TIME, mLastOrganizerCheck)
-            .putString(PREF_ORGANIZER_CHECK_ID, mCheckedId)
+        preferences.edit()
+            .putLong(PREF_ORGANIZER_CHECK_TIME, lastOrganizerCheckTimeStamp)
+            .putString(PREF_ORGANIZER_CHECK_ID, checkedId)
             .putBoolean(PREF_ORGANIZER_STATE, isOrganizer())
             .apply();
     }
