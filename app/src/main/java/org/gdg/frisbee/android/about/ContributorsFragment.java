fbbdc66ade5509f266b1ad553c72bcbef5e9161f
Said Tahsin Dane <tasomaniac@gmail.com>
2016-09-18 21:36:01 +0200

    Rename GITHUB_ORGA to GIHUB_ORGANIZATION

--- app/src/main/java/org/gdg/frisbee/android/about/ContributorsFragment.java
+++ app/src/main/java/org/gdg/frisbee/android/about/ContributorsFragment.java
@@ -32,7 +32,7 @@ import org.joda.time.DateTime;
 
 public class ContributorsFragment extends PeopleListFragment {
 
-    private static final String GITHUB_ORGA = "gdg-x";
+    private static final String GITHUB_ORGANIZATION = "gdg-x";
     private static final String GITHUB_REPO = "frisbee";
 
     @Override
@@ -65,7 +65,7 @@ public class ContributorsFragment extends PeopleListFragment {
     }
 
     private void fetchGitHubContributors() {
-        App.getInstance().getGithub().getContributors(GITHUB_ORGA, GITHUB_REPO)
+        App.getInstance().getGithub().getContributors(GITHUB_ORGANIZATION, GITHUB_REPO)
             .enqueue(new Callback<ContributorList>() {
                 @Override
                 public void success(final ContributorList contributors) {
